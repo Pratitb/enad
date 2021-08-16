21a9d908ec5d008bd7f4d223bc2831d4a2caca79
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2021-08-16 17:46:48 +0530

    updates

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,61 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+$(document).ready(function() {
+    $(".home-slider").owlCarousel({
         stagePadding: 0,
         margin: 0,
-        
+
         items: 1,
         loop: true,
         autoplay: true,
         autoplaySpeed: 2000,
         autoplayTimeout: 5000,
         autoplayHoverPause: true,
+
         nav: true,
-        navText: ["<span>&larr;</span>", "<span>&rarr;</span>"],
+        mouseDrag: false,
+
+        navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"],
         center: true
     });
+    $(".projects").owlCarousel({
+      stagePadding: 0,
+      margin: 0,
+
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      
+      /* nav: true, */
+      mouseDrag: false,
+
+      /* navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"], */
+      center: true
   });
+});
 
-    AOS.init({
-        duration: 3000
-    });
+AOS.init({
+    duration: 3000
+});
 
-  const counters = document.querySelectorAll('.counter-number');
-  const speed = 500;
+const counters = document.querySelectorAll('.counter-number');
+const speed = 500;
 
-  counters.forEach(counter =>{
-      const updateCount = () => {
-          const target = +counter.getAttribute('data-target');
-          const count = +counter.innerText;
-          const increment = target/speed;
-          
-          
-          //now we increment the number to target
-          if(count<target){
-             counter.innerText = Math.ceil(count + increment);
-             setTimeout(updateCount);
-          }
-          else{
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / speed;
+
+
+        //now we increment the number to target
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount);
+        } else {
             count.innerText = target;
-          }
-      }
-      updateCount();
-  })
+        }
+    }
+    updateCount();
+})
