f47cec292d76268aa54846566ec6ab97bf949057
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2021-09-06 12:08:33 +0530

    responsive 1

--- js/custom.js
+++ js/custom.js
@@ -1,61 +1,51 @@
-$(document).ready(function() {
-    $(".home-slider").owlCarousel({
-        stagePadding: 0,
-        margin: 0,
+let menu = document.querySelector(".mobile-menu");
+let scrollPos = 0;
+window.addEventListener("scroll", function () {
+  if (document.body.getBoundingClientRect().top < scrollPos) {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+  scrollPos = document.body.getBoundingClientRect().top;
+});
 
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true,
+$(document).ready(function () {
+  $(".home-slider").owlCarousel({
+    stagePadding: 0,
+    margin: 0,
 
-        nav: true,
-        mouseDrag: false,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
 
-        navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"],
-        center: true
-    });
-    $(".projects").owlCarousel({
-      stagePadding: 0,
-      margin: 0,
+    nav: true,
+    mouseDrag: false,
 
-      items: 1,
-      loop: true,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      autoplayTimeout: 4000,
-      autoplayHoverPause: true,
-      
-      nav: true,
-      mouseDrag: false,
+    navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"],
+    center: true,
+  });
+  $(".projects").owlCarousel({
+    stagePadding: 0,
+    margin: 0,
 
-      /* navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"], */
-      center: true
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+
+    nav: true,
+    mouseDrag: false,
+
+    /* navText: ["<span>&#8249;</span>", "<span>&#8250;</span>"], */
+    center: true,
   });
 });
 
 AOS.init({
-    duration: 3000
+  duration: 1000,
 });
-
-const counters = document.querySelectorAll('.counter-number');
-const speed = 500;
-
-counters.forEach(counter => {
-    const updateCount = () => {
-        const target = +counter.getAttribute('data-target');
-        const count = +counter.innerText;
-        const increment = target / speed;
-
-
-        //now we increment the number to target
-        if (count < target) {
-            counter.innerText = Math.ceil(count + increment);
-            setTimeout(updateCount);
-        } else {
-            count.innerText = target;
-        }
-    }
-    updateCount();
-})
