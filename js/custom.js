ecdbf8767f4b16ad0a14388e3be5d6a98d0ff2b9
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2021-09-16 17:31:34 +0530

    almost there - styling, research page, favicon

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,12 @@ $(document).ready(function () {
 AOS.init({
   duration: 1000,
 });
+
+function externalLinks() {
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++)
+  {
+      var b = c[a];
+      b.getAttribute('href') && b.hostname !== location.hostname &&(b.target = '_blank')
+  }
+}
+externalLinks();
